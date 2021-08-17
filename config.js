a14fee106dff52ce89686fa0f76e15e58a39b61a
Darko174 <55543529+Darko174@users.noreply.github.com>
2021-08-17 21:29:34 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const config = {
-    "BOT_TOKEN": "ODU3MjIxOTc0MTQwMzg3MzQ4.YNMcLQ.ccZG6XiYkrxPxfrG1_0OhL8GpmQ"
+    "BOT_TOKEN": "ODU3MjIxOTc0MTQwMzg3MzQ4.YNMcLQ.RShLR2lSqcliP1eb5etYivZF6zA"
 }
 export default config
